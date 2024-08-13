c49d896c833beb615e9322e9a216af88965b7b36
mohammedshanan <mohammedshana12@gmail.com>
2024-08-13 20:51:04 +0300

    inital commit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import requests
+
+"""
+Script that queries subscribers for a given Reddit subreddit.
+"""
+
+
+def number_of_subscribers(subreddit):
+    """Return the total number of subscribers for a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(url)
+    if response.status_code == 200:
+        return int(response.json()["data"]["subscribers"])
+    else:
+        return 0
