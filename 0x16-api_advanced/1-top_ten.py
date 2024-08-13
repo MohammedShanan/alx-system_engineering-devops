60ecc565de435632c7b14bf2eeadbe89871a098b
mohammedshanan <mohammedshana12@gmail.com>
2024-08-13 22:29:01 +0300

    solved top 10

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,8 +8,8 @@ def top_ten(subreddit):
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {"User-Agent": "u/trigun"}
     params = {"limit": 10}
-    response = requests.get(url, headers=headers, 
-                        params=params, allow_redirects=False)
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
 
     if response.status_code == 200:
         results = response.json().get("data")
