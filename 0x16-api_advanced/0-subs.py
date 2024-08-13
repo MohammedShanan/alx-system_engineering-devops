aa0c35889ce356fbee53abd4f7705b66f3954462
mohammedshanan <mohammedshana12@gmail.com>
2024-08-13 20:58:35 +0300

    inital commit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,7 @@ import requests
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers for a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = requests.get(url)
+    response = requests.get(url, allow_redirects=False)
     if response.status_code == 200:
         return int(response.json()["data"]["subscribers"])
     else:
